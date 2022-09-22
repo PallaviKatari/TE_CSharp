c17e15db40641e2d1ac57b945e58817666ad7d66
PallaviKatari <pallavikatari@gmail.com>
2022-09-22 15:59:48 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;  //Namespace - Collection of classes -> collection of functions/methods
+
+/// <summary>
+/// user-defined namespace
+/// </summary>
+namespace Basic_Concepts
+{
+    /// <summary>
+    /// Basic C-Sharp Concepts
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// entry point ,static - invoked once,void - returns nothing
+        /// </summary>
+        public static void Main()
+        {
+            //Console->class
+            Console.WriteLine("Welcome to C-Sharp");
+
+        }
+    }
+
+}
